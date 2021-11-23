0531995fbf010314d6d527d6f6f4da645dc2ddf9
Aditya Neve <aditya.neve@gmail.com>
2021-11-23 10:36:43 +0530

    next and prev page buttons added, search api working, navbar updated

--- src/App.js
+++ src/App.js
@@ -4,22 +4,54 @@ import Movie from "./components/Movie";
 import VideoPopup from "./components/VideoPopup";
 
 const FEATURED_API =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=14dc73a4bd1abf7c14d4209c112b4496&page=1";
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=14dc73a4bd1abf7c14d4209c112b4496&page=";
 const SEARCH_API =
     "https://api.themoviedb.org/3/search/movie?&api_key=14dc73a4bd1abf7c14d4209c112b4496&query=";
+const POPULAR_API =
+    "https://api.themoviedb.org/3/movie/popular?api_key=14dc73a4bd1abf7c14d4209c112b4496&language=en-US&page=";
+const NOW_PLAYING =
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=14dc73a4bd1abf7c14d4209c112b4496&language=en-US&page=";
+const TOP_RATED_API =
+    "https://api.themoviedb.org/3/movie/top_rated?api_key=14dc73a4bd1abf7c14d4209c112b4496&language=en-US&page=";
+const UPCOMING_API =
+    "https://api.themoviedb.org/3/movie/upcoming?api_key=14dc73a4bd1abf7c14d4209c112b4496&language=en-US&page=";
 
 function App() {
     const [movies, setMovies] = useState([]);
+    const [API, setAPI] = useState(
+        "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=14dc73a4bd1abf7c14d4209c112b4496&page="
+    );
     const [searchQuery, setSearchQuery] = useState("");
     const [searchTrailerId, setSearchTrailerId] = useState("");
     const [trailerKey, setTrailerKey] = useState("");
     const [buttonPopup, setButtonPopup] = useState(false);
+    const [page, setPage] = useState(1);
+    const [searchPage, setSearchPage] = useState(1);
 
-    const getMovies = (API) => {
+    const getMovies = () => {
         axios
-            .get(API)
+            .get(API + page)
             .then((response) => {
                 setMovies(response.data.results);
+                if (response.data.results.length === 0) {
+                    alert("No Movies Found.");
+                    handleDecrement();
+                }
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+    };
+
+    const searchMovies = (searchQuery) => {
+        axios
+            .get(SEARCH_API + searchQuery + "&page=" + searchPage)
+            .then((response) => {
+                setMovies(response.data.results);
+                if (response.data.results.length === 0) {
+                    alert("No Movies Found.");
+                    handleDecrement();
+                }
             })
             .catch((error) => {
                 alert(error.message);
@@ -28,38 +60,112 @@ function App() {
 
     const getTrailer = (searchTrailerId) => {
         axios
-            .get(`https://api.themoviedb.org/3/movie/${searchTrailerId}/videos?api_key=14dc73a4bd1abf7c14d4209c112b4496&language=en-US`)
+            .get(
+                `https://api.themoviedb.org/3/movie/${searchTrailerId}/videos?api_key=14dc73a4bd1abf7c14d4209c112b4496&language=en-US`
+            )
             .then((response) => {
-                setTrailerKey(response.data.results[0].key);
-                return true;
+                let flag = true;
+                for (let i = 0; i < response.data.results.length; i++) {
+                    if (response.data.results[i].type === "Teaser") {
+                        setTrailerKey(response.data.results[i].key);
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag === true) {
+                    setTrailerKey(response.data.results[0].key);
+                }
             })
             .catch((error) => {
-                alert(error.message)
-                return false;
-            })
-    }
+                alert(error.message);
+            });
+    };
 
     useEffect(() => {
-        getMovies(FEATURED_API);
-    }, []);
+        getMovies();
+    }, [API, page]);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
+    useEffect(() => {
         if (searchQuery) {
-            getMovies(SEARCH_API + searchQuery);
-            setSearchQuery("");
+            searchMovies(searchQuery);
         }
-    };
+    }, [searchQuery, searchPage]);
 
     const handleOnChange = (event) => {
         setSearchQuery(event.target.value);
     };
 
+    const handleIncrement = () => {
+        if (!searchQuery) {
+            setPage((prevPage) =>
+                prevPage >= 1 ? prevPage + 1 : (prevPage = 1)
+            );
+        } else if (searchQuery) {
+            setSearchPage((prevPage) =>
+                prevPage >= 1 ? prevPage + 1 : (prevPage = 1)
+            );
+        }
+    };
+
+    const handleDecrement = () => {
+        if (!searchQuery) {
+            setPage((prevPage) =>
+                prevPage > 1 ? prevPage - 1 : (prevPage = 1)
+            );
+        } else if (searchQuery) {
+            setSearchPage((prevPage) =>
+                prevPage > 1 ? prevPage - 1 : (prevPage = 1)
+            );
+        }
+    };
+
     return (
         <>
             <header>
-                <form onSubmit={handleSubmit}>
+                <form className="navForm">
+                    <ul className="navItems">
+                        <li
+                            onClick={() => {
+                                setPage(1);
+                                setAPI(POPULAR_API);
+                                getMovies();
+                                setSearchQuery("");
+                            }}
+                        >
+                            Popular
+                        </li>
+                        <li
+                            onClick={() => {
+                                setPage(1);
+                                setAPI(TOP_RATED_API);
+                                getMovies();
+                                setSearchQuery("");
+                            }}
+                        >
+                            Top Rated
+                        </li>
+                        <li
+                            onClick={() => {
+                                setPage(1);
+                                setAPI(NOW_PLAYING);
+                                getMovies();
+                                setSearchQuery("");
+                            }}
+                        >
+                            Now Playing
+                        </li>
+                        <li
+                            onClick={() => {
+                                setPage(1);
+                                setAPI(UPCOMING_API);
+                                getMovies();
+                                setSearchQuery("");
+                            }}
+                        >
+                            Upcoming
+                        </li>
+                    </ul>
                     <input
                         className="search"
                         type="text"
@@ -71,19 +177,42 @@ function App() {
             </header>
             <div className="movieContainer">
                 {movies.length > 0 &&
-                    movies.map((movie) => <Movie key={movie.id} {...movie} setButtonPopup={setButtonPopup} setSearchTrailerId={setSearchTrailerId} />)}
+                    movies.map((movie) => (
+                        <Movie
+                            key={movie.id}
+                            {...movie}
+                            setButtonPopup={setButtonPopup}
+                            setSearchTrailerId={setSearchTrailerId}
+                        />
+                    ))}
 
-                    {
-                        (buttonPopup) ? (
-                            <VideoPopup trigger={buttonPopup} setButtonPopup={setButtonPopup}>
-                                {getTrailer(searchTrailerId)}
-                                <iframe className="videoContainer" type="text/html" height="100%" width="100%"
-                                    src={`https://www.youtube.com/embed/${trailerKey}?autoplay=1&controls=1`} allow="autoplay; encrypted-media" frameBorder="0" allowFullScreen>
-                                </iframe>
-                            </VideoPopup>
-                        ) : null
-                    }
-                
+                {buttonPopup && searchTrailerId ? (
+                    <VideoPopup
+                        trigger={buttonPopup}
+                        setButtonPopup={setButtonPopup}
+                    >
+                        {getTrailer(searchTrailerId)}
+                        <iframe
+                            key={searchTrailerId}
+                            className="videoContainer"
+                            type="text/html"
+                            height="100%"
+                            width="100%"
+                            src={`https://www.youtube.com/embed/${trailerKey}?autoplay=1&controls=1`}
+                            allow="autoplay; encrypted-media;"
+                            frameBorder="0"
+                            allowFullScreen
+                        ></iframe>
+                    </VideoPopup>
+                ) : null}
+                <div className="nextAndPrevBtn">
+                    <button onClick={handleDecrement}>
+                        PREV PAGE {searchQuery ? searchPage : page}
+                    </button>
+                    <button onClick={handleIncrement}>
+                        NEXT PAGE {searchQuery ? searchPage : page}
+                    </button>
+                </div>
             </div>
         </>
     );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -21,13 +21,13 @@ const Movie = ({
     poster_path: posterPath,
     overview,
     vote_average: voteAverage,
+    release_date: releaseDate,
 }) => (
     <div className="movie">
         <div className="movieHeader">
             <VideocamSharp
                 className="getTrailer"
                 color={"#00000"}
-                // shake
                 height="50px"
                 width="50px"
                 onClick={() => {
@@ -35,6 +35,7 @@ const Movie = ({
                     setButtonPopup(true);
                 }}
             />
+            <span className="releaseDate">{releaseDate}</span>
             <img
                 src={
                     posterPath
@@ -42,6 +43,7 @@ const Movie = ({
                         : "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1459&q=80"
                 }
                 alt="title"
+                placeholder={releaseDate}
             />
             <div className="movieInfo">
                 <h3>{title}</h3>
